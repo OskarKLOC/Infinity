01fbe4e406445eaa56da30f32071a34f59910093
BeletteCorp <94044832+MoreauLeo@users.noreply.github.com>
2022-09-21 11:07:19 +0200

    birthdate

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -27,21 +27,10 @@ class UserType extends AbstractType
             ->add('password')
             ->add('lastname')
             ->add('firstname')
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-            // ->add('birthdate')
-            ->add('birthdate', BirthdayType::class, [
-                'choice' => 'widget',
-=======
-            ->add('birthdate', BirthdayType::class, [
->>>>>>> Stashed changes
-                'years' => range(1, 200),
-=======
             ->add('birthdate', BirthdayType::class, [
                 //'widget' => 'choice',
                 'years' => range(1904, 2022),
                 'format' => 'dd/MM/yyyy'
->>>>>>> e6fc582b4bb3b76ca908bcc4c8d4761778a5c572
             ])
             ->add('birth_city')
             ->add('phone_number')
